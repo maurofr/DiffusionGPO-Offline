0b0c4ce9268ba8aa9db1a5bcca23d7f9c079e36a
Mauro Filomeno Rivero <m.filomenor@gmail.com>
2025-10-11 21:36:22 +0200

    Update upload_model_to_hub.py

--- upload_model_to_hub.py
+++ upload_model_to_hub.py
@@ -1,11 +1,23 @@
-from huggingface_hub import upload_folder, upload_file
-import sys
+from huggingface_hub import upload_file
 
-d = 'path/to/local/ckpt/unet/folder'
-for fname in ['config.json', 'diffusion_pytorch_model.safetensors']:
-    print(f"Uploading {fname}...")
-    upload_file(path_or_fileobj=d + fname,
-                path_in_repo=fname,
-                repo_id = "<hf-org>/<make a repo and add it here>", # please don't overwrite other files in repo
-                token=sys.argv[1] # Add your HF token here or just feed in via command line
-               )
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hf_token', type=str, required=True, help="Hugging Face token for authentication")
+    parser.add_argument('--local_path', type=str, required=True, help="Path to local unet checkpoint")
+    parser.add_argument('--repo_id', type=str, required=True, help="Huggingface repo ID")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    for fname in ['config.json', 'diffusion_pytorch_model.safetensors']:
+        print(f"Uploading {fname}...")
+        upload_file(path_or_fileobj= args.local_path + fname,
+                    path_in_repo=fname,
+                    repo_id = args.repo_id, 
+                    token=args.hf_token 
+                )
+        
+if __name__ == "__main__":
+    main()
